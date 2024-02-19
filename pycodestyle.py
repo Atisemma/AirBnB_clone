fc7153993eed062fb5a18ba34cb388afa262c227
Atisemma <atisemma26@gmail.com>
2024-02-19 12:57:01 -0800

    pycodestyle compliance

---
+++ pycodestyle.py
@@ -0,0 +1,13 @@
+def calculate_sum(a, b):
+    """
+    Calculate the sum of two numbers.
+
+    Parameters;
+        a(int): The first number.
+        b(int): The second number.
+
+    Returns:
+        int: The sum of two numbers.
+    """
+
+    return a + b
